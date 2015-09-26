7fa93ace943510fc3cb7bb3b32b20100cdffd118
Johan Segerfeldt <johan.segerfeldt@me.com>
2015-09-26 22:23:46 +0200

    Remove Locale field

--- src/kata/Calculator.java
+++ src/kata/Calculator.java
@@ -5,19 +5,16 @@ import java.util.Locale;
 import java.util.Objects;
 
 public class Calculator {
-	private Locale locale = Locale.getDefault();
+	private DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.getDefault());
 	private String display = "";
 
 	public String getDisplay() {
 		return display;
 	}
 
-	public Locale getLocale() {
-		return locale;
-	}
-
-	public void setLocale(Locale locale) {
-		this.locale = locale;
+	// Only intended for testing.
+	void setLocale(Locale locale) {
+		dfs = new DecimalFormatSymbols(locale);
 	}
 
 	public void enterDigit(String digit) {
@@ -27,7 +24,6 @@ public class Calculator {
 	public void enterDecimalPointer() {
 		if (Objects.equals(display, ""))
 			display = "0";
-		DecimalFormatSymbols dfs = new DecimalFormatSymbols(getLocale());
 		display += dfs.getDecimalSeparator();
 	}
 
