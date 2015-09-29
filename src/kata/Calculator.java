c6085ff1ebb0c9db44d370a38cc8b10f8be07098
Johan Segerfeldt <johan.segerfeldt@me.com>
2015-09-29 04:44:15 +0200

    Rename variables

--- src/kata/Calculator.java
+++ src/kata/Calculator.java
@@ -1,13 +1,13 @@
 package kata;
 
 public class Calculator {
-	static final Operator IDENTITY_OPERATOR = lhs -> lhs;
+	static final Operator IDENTITY_OPERATOR = operand -> operand;
 
 	private Operator deferredOperator = IDENTITY_OPERATOR;
 	private double value = 0.0;
 
-	public void enter(double value) {
-		this.value = value;
+	public void enter(double operand) {
+		value = operand;
 	}
 
 	public double calculate() {
@@ -16,21 +16,21 @@ public class Calculator {
 		return value;
 	}
 
-	public void add(double value) {
-		double operand = this.value;
-		this.value = deferredOperator.call(value);
+	public void add(double term) {
+		double operand = value;
+		value = deferredOperator.call(term);
 		deferredOperator = x -> operand + x;
 	}
 
-	public void multiply(double value) {
-		this.value *= value;
+	public void multiply(double factor) {
+		value *= factor;
 	}
 
-	public void subtract(double value) {
-		add(-value);
+	public void subtract(double term) {
+		add(-term);
 	}
 
-	public void divide(double value) {
-		this.value /= value;
+	public void divide(double quotient) {
+		value /= quotient;
 	}
 }
