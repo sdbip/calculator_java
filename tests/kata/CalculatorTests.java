118f991b4660b659b0a15d27ddcab07acb3cca38
Johan Segerfeldt <johan.segerfeldt@me.com>
2015-09-29 04:39:00 +0200

    Create the calculator in setUp

--- tests/kata/CalculatorTests.java
+++ tests/kata/CalculatorTests.java
@@ -1,21 +1,27 @@
 package kata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTests {
 
+	private Calculator calculator;
+
+	@Before
+	public void setUp() throws Exception {
+		calculator = new Calculator();
+	}
+
 	@Test
 	public void remembersEnteredValue() {
-		Calculator calculator = new Calculator();
 		calculator.enter(2.0);
 		assertEquals(2.0, calculator.getValue(), 0);
 	}
 
 	@Test
 	public void knowsHowToAdd() {
-		Calculator calculator = new Calculator();
 		calculator.enter(2.0);
 		calculator.add(1.5);
 		assertEquals(3.5, calculator.getValue(), 0);
@@ -23,7 +29,6 @@ public class CalculatorTests {
 
 	@Test
 	public void knowsHowToMultiply() {
-		Calculator calculator = new Calculator();
 		calculator.enter(2.0);
 		calculator.multiply(0.5);
 		assertEquals(1.0, calculator.getValue(), 0);
