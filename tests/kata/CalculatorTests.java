dc52018df072fb780974e1d227728a7ebccc1863
Johan Segerfeldt <johan.segerfeldt@me.com>
2015-10-06 15:01:58 +0200

    Extract field :: Initialize in setUp()

--- tests/kata/CalculatorTests.java
+++ tests/kata/CalculatorTests.java
@@ -1,12 +1,19 @@
 package kata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTests {
 
+	private Calculator calculator;
+
+	@Before
+	public void setUp() throws Exception {
+		calculator = new Calculator();
+	}
+
 	@Test
 	public void canEnterDigits() {
-		Calculator calculator = new Calculator();
 	}
 
 }
